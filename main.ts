da75e972a4ac3eca9ad7e202e28ec8623ab61671
pablo g. rubio <pagoru@gmail.com>
2020-05-15 16:16:56 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,2 @@
-export const foo = () => 'Hello land!';
-export const fighters = (num: number) => num * 2;
+export const foo = () => 'Hello land 2.0.0!';
+export const fighters = (num: number) => num * 3;
